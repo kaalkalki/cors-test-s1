0c9a4fcfc37f832e21b179e8dfea156a371e504f
kaalkalki <kunalkalki95@gmail.com>
2019-12-10 23:56:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var app = require('express')();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+//app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    console.log("-------------------------------Request recieved!!!--------------------");
+    console.log(req);
+    console.log(res);
+    next();
+});
+
+app.get('/hello', function (req, res) {
+    res.json({ "hello": "hello" });
+});
+app.use(function (req, res, next) {
+    res.status(404).json({"reason":"No resource exist"});
+});
+var server = app.listen(3000, 'localhost', function () {
+    console.log(`${server.address().address} :${server.address().port}`);
+});
